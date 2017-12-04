d85a051b2c4d866d0a8549793bbc049eaeb15a7a
jirwin <jordan.s.irwin@gmail.com>
2017-12-03 23:09:57 -0800

    Initial commit

---
+++ Assets/Scripts/RollDice.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RollDice : MonoBehaviour {
+
+	public int TotalRoll;
+	public Texture[] DiceTexturesForZero;
+	public Texture[] DiceTexturesForOne;
+	public GameObject DiceContainer;
+
+	private int maxDice = 4;
+	private int[] rolls;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RollTheDice() {
+
+		TotalRoll = 0;
+		rolls = new int[maxDice];
+		for (int i = 0; i < maxDice; i++) {
+			rolls[i] = Random.Range (0, 2);
+			TotalRoll += rolls [i];
+		}
+
+		Debug.Log ("Dice roll was " + TotalRoll);
+		AssignDiceArt ();
+	}
+		
+	private void AssignDiceArt() {
+		var images = DiceContainer.GetComponentsInChildren<RawImage> ();
+		for (int i = 0; i < maxDice; i++) {
+			if(rolls[i] == 0) {
+				images[i].texture = DiceTexturesForZero[Random.Range(0, DiceTexturesForZero.Length - 1)];
+			}
+			else  {
+				images[i].texture = DiceTexturesForOne[Random.Range(0, DiceTexturesForOne.Length - 1)];
+			}	
+		}
+	}
+}
---
+++ Assets/Scripts/StoneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneController : MonoBehaviour {
+
+	public TileController StartingTile;
+
+	private TileController currentTile;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnMouseUp() {
+		Debug.Log ("Clicked!");
+
+		TileController nextTile;
+		if (currentTile == null) {
+			nextTile = StartingTile;
+		} else {
+			nextTile = currentTile.NextTiles[0];
+		}
+
+		transform.position = nextTile.gameObject.transform.position;
+		currentTile = nextTile;
+	}
+}
