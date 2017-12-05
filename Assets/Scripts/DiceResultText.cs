18bb974e6f9f04be6f5f073c949c3c54ab61ac23
jirwin <jordan.s.irwin@gmail.com>
2017-12-04 22:50:49 -0800

    Add dice result text to UI

---
+++ Assets/Scripts/DiceResultText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceResultText : MonoBehaviour {
+
+	private RollDice rollDice;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		rollDice = GameObject.FindObjectOfType<RollDice> ();
+		text = FindObjectOfType<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "= " + rollDice.TotalRoll;
+	}
+}
